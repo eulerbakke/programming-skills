4f5c94ee8e8acb7c920317b4c3d958514f07011b
eulerbakke <euler.bakke@gmail.com>
2021-08-20 12:24:51 -0300

    Updating Q3 files

--- Questions/Q3/Readme.txt
+++ Questions/Q3/Readme.txt
@@ -1,1 +1,17 @@
+    
+Initially, compare both strings starting from the first characters of them, until find a difference. This index is where
+we need to start removing from the s string and also where we should start moving from t string to s string.
+From this information, we extract what is called perfectK, which is the value of the exact number of moves needed to make the
+conversion, without using removals on empty strings to spend the available moves.
+Finally, the code compares the moves needed and the perfectK and return yes or no based on this conditions.
+
+***--- Important to note: ---***
+There is a scenario that it's not clear in the problem description if we should take into account:
+-> Even though the string is not empty, we can do multiple removals/concats if (k - perfectK)%2 == 0) to spend the additional moves.
+In this case, we would be removing and adding the same character to the string just to spend the additional moves (even though they were
+already the same).
+The question examples are incomplete, but I've just considered the empty string scenario, as was described in the problem.
+To tako it into account (together with the empty string case), we could just add this condition in the last else: ((k - perfectK)%2 == 0).
+
+
 Author: Euler Bakke
--- Questions/Q3/q3.cpp
+++ Questions/Q3/q3.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+/* -- Brief description of the solution:
+*       Initially, compare both strings starting from the first characters of them, until find a difference. This index is where
+*       we need to start removing from the s string and also where we should start moving from t string to s string.
+*       From this information, we extract what is called perfectK, which is the value of the exact number of moves needed to make the
+*       conversion, without using removals on empty strings to spend the available moves.
+*       Finally, the code compares the moves needed and the perfectK and return yes or no based on this conditions.
+*
+*       ***--- Important to note: ---***
+*       There is a scenario that it's not clear in the problem description if we should take into account:
+*       -> Even though the string is not empty, we can do multiple removals/concats if (k - perfectK)%2 == 0) to spend the additional moves.
+*       In this case, we would be removing and adding the same character to the string just to spend the additional moves (even though they were
+*       already the same).
+*       The question examples are incomplete, but I've just considered the empty string scenario, as was described in the problem.
+*       To tako it into account (together with the empty string case), we could just add this condition in the last else: ((k - perfectK)%2 == 0).
+*/
+
+// Function prototype
 string ConcatRemove(string, string, int);
 
 int main()
@@ -54,18 +71,10 @@ string ConcatRemove(string s, string t, int k)
     // Number of removals needed to string s
     sRemovals = s.length() - index;
 
-    cout << "THEY START TO BE DIFFERENT AT INDEX " << index << "\n";
-
-    cout << "NOW I NEED TO REMOVE " << sRemovals << "!\n";
-
     // Now, calculating how many characters need to be added from t to s
     numConcats = t.length() - index;
 
     perfectK = sRemovals + numConcats;
-
-    cout << "AND THEN A I NEED TO CONCAT " << numConcats << " times!\n";
-
-    cout << "The perfect k here is: " << perfectK << "!\n";
 
     if (k < (perfectK))
     {
@@ -76,18 +85,11 @@ string ConcatRemove(string s, string t, int k)
         return "yes";
     }
     else
-    { // This else is for the case of additional movements available, checking only for empty string
+    { // This else is for the case of additional movements available, checking only for empty string to spend the additional moves.
         if (!index)
         {
             return "yes";
         }
+        return "no";
     }
-
-    /* There is a scenario that it's not clear in the problem description if we should take into account:
-    *  -> Even though the string is not empty, we can do multiple removals/concats if (k - perfectK)%2 == 0) to spend the additional moves.
-    *  In this case, we would be removing and adding the same character to the string just to spend the additional moves (even though they were
-    *  already the same).
-    *  The question examples are incomplete, but I've just considered the empty string scenario, as was described in the problem.
-    *  To tako it into account, we could just add this condition in the last else: ((k - perfectK)%2 == 0).
-    */
 }
